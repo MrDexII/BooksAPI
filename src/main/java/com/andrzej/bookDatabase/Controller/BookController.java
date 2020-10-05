ee24d85f089fafbc862866cf1985cd17a99a2326
MrDexII <free.andrew.a@gmail.com>
2020-10-05 10:50:52 +0200

    refactoring book controller

--- src/main/java/com/andrzej/bookDatabase/Controller/BookController.java
+++ src/main/java/com/andrzej/bookDatabase/Controller/BookController.java
@@ -11,89 +11,46 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.validation.Valid;
-import java.util.List;
 
 @Controller
 @RequestMapping("/book")
 public class BookController {
-
-    private BookService bookService;
+    private final BookService bookService;
 
     @Autowired
     public BookController(BookService bookService) {
         this.bookService = bookService;
     }
 
+    //SHOW VIEW
     @RequestMapping(value = {"/", "/new"}, method = RequestMethod.GET)
     public ModelAndView showAddBookForm() {
-        ModelAndView modelAndView = new ModelAndView();
-        Book book = new Book();
-//        Link link = linkTo(BookController.class).slash(book).withRel("book");
-        modelAndView.addObject("book", book);
-        modelAndView.setViewName("book/new");
-        return modelAndView;
-    }
-
-    @RequestMapping(value = "/new", method = RequestMethod.POST)
-    public ModelAndView createBook(@Valid Book book, BindingResult result) {
-        ModelAndView modelAndView = new ModelAndView();
-        if (result.hasErrors()) {
-            modelAndView.setViewName("book/new");
-        } else {
-            Book bookExists = bookService.findBookByIsbn(book.getIsbn());
-            if (bookExists != null) {
-                result
-                        .rejectValue("isbn", "error.isbn",
-                                "There is already a book registered with this isbn");
-                modelAndView.setViewName("book/new");
-                return modelAndView;
-            }
-            bookService.saveBook(book);
-            modelAndView.addObject("successMessage", "Book has been registered successfully");
-            modelAndView.addObject("book", new Book());
-            modelAndView.setViewName("admin/home");
-        }
-        return modelAndView;
-    }
-
-    @RequestMapping(value = "/show", method = RequestMethod.GET)
-    public ModelAndView showBooks() {
-        ModelAndView modelAndView = new ModelAndView();
-        List<Book> allBooks = bookService.findAllBooks();
-        modelAndView.addObject("allBooks", allBooks);
-        modelAndView.setViewName("book/show");
-        return modelAndView;
+        return bookService.showAddBookForm();
     }
 
     @RequestMapping(value = "/update/{id}", method = RequestMethod.GET)
     public ModelAndView showUpdateView(@PathVariable("id") long id) {
-        ModelAndView modelAndView = new ModelAndView();
-        Book book = bookService.findBookById(id);
-        modelAndView.addObject("book", book);
-        modelAndView.setViewName("book/update");
-        return modelAndView;
+        return bookService.showUpdateView(id);
+    }
+
+    //CRUD
+    @RequestMapping(value = "/new", method = RequestMethod.POST)
+    public ModelAndView createBook(@Valid Book book, BindingResult result) {
+        return bookService.saveBook(book, result);
+    }
+
+    @RequestMapping(value = "/show", method = RequestMethod.GET)
+    public ModelAndView showBooks() {
+        return bookService.findAllBooks();
     }
 
     @RequestMapping(value = "/update/{id}", method = RequestMethod.PUT)
     public ModelAndView updateBook(@PathVariable("id") long id, Book book, BindingResult result) {
-        ModelAndView modelAndView = new ModelAndView();
-        if (result.hasErrors()) {
-            modelAndView.setViewName("book/update");
-        } else {
-            bookService.updateBook(id, book);
-            modelAndView.addObject("successMessage", "Book has been updated successfully");
-            modelAndView.addObject("book", new Book());
-            modelAndView.setViewName("admin/home");
-        }
-        return modelAndView;
+        return bookService.updateBook(id, book, result);
     }
 
     @RequestMapping(value = "/delete/{id}", method = RequestMethod.DELETE)
     public ModelAndView deleteBook(@PathVariable("id") long id) {
-        ModelAndView modelAndView = new ModelAndView();
-        bookService.deleteBook(id);
-        modelAndView.addObject("successMessage", "Book has been successfully delete");
-        modelAndView.setViewName("/admin/home");
-        return modelAndView;
+        return bookService.deleteBook(id);
     }
 }
--- src/main/java/com/andrzej/bookDatabase/Service/BookService.java
+++ src/main/java/com/andrzej/bookDatabase/Service/BookService.java
@@ -3,40 +3,84 @@ package com.andrzej.bookDatabase.Service;
 import com.andrzej.bookDatabase.Model.Book;
 import com.andrzej.bookDatabase.Repository.BookRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
 @Service()
 public class BookService {
-
-
-    private BookRepository repository;
+    private final BookRepository repository;
 
     public BookService(BookRepository repository) {
         this.repository = repository;
     }
 
-    public void saveBook(Book book) {
-        repository.save(book);
+    public ModelAndView saveBook(Book book, BindingResult result) {
+        ModelAndView modelAndView = new ModelAndView();
+        if (result.hasErrors()) {
+            modelAndView.setViewName("book/new");
+        } else {
+            Book bookExists = repository.findByIsbn(book.getIsbn());
+            if (bookExists != null) {
+                result
+                        .rejectValue("isbn", "error.isbn",
+                                "There is already a book registered with this isbn");
+                modelAndView.setViewName("book/new");
+                return modelAndView;
+            }
+            repository.save(book);
+            modelAndView.addObject("successMessage", "Book has been registered successfully");
+            modelAndView.addObject("book", new Book());
+            modelAndView.setViewName("admin/home");
+        }
+        return modelAndView;
     }
 
-    public void updateBook(long id, Book book) {
-        book.setId(id);
-        repository.save(book);
+    public ModelAndView updateBook(long id, Book book, BindingResult result) {
+        ModelAndView modelAndView = new ModelAndView();
+        if (result.hasErrors()) {
+            modelAndView.setViewName("book/update");
+        } else {
+            book.setId(id);
+            repository.save(book);
+            modelAndView.addObject("successMessage", "Book has been updated successfully");
+            modelAndView.addObject("book", new Book());
+            modelAndView.setViewName("admin/home");
+        }
+        return modelAndView;
     }
 
-    public Book findBookById(long id) {
-        return repository.findById(id);
-    }
-    public Book findBookByIsbn(String isbn){
-        return repository.findByIsbn(isbn);
-    }
-
-    public void deleteBook(long id) {
+    public ModelAndView deleteBook(long id) {
+        ModelAndView modelAndView = new ModelAndView();
         repository.deleteById(id);
+        modelAndView.addObject("successMessage", "Book has been successfully delete");
+        modelAndView.setViewName("/admin/home");
+        return modelAndView;
     }
 
-    public List<Book> findAllBooks() {
-        return repository.findAll();
+    public ModelAndView findAllBooks() {
+        ModelAndView modelAndView = new ModelAndView();
+        List<Book> allBooks = repository.findAll();
+        modelAndView.addObject("allBooks", allBooks);
+        modelAndView.setViewName("book/show");
+        return modelAndView;
+    }
+
+    public ModelAndView showAddBookForm() {
+        ModelAndView modelAndView = new ModelAndView();
+        Book book = new Book();
+//        Link link = linkTo(BookController.class).slash(book).withRel("book");
+        modelAndView.addObject("book", book);
+        modelAndView.setViewName("book/new");
+        return modelAndView;
+    }
+
+    public ModelAndView showUpdateView(long id) {
+        ModelAndView modelAndView = new ModelAndView();
+        Book book = repository.findById(id);
+        modelAndView.addObject("book", book);
+        modelAndView.setViewName("book/update");
+        return modelAndView;
     }
 }
